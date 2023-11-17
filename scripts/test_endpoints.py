b7512492d39f6c4308da4eaf4d02c379d86ac24f
Neelzee <nilsien2001@gmail.com>
2023-11-17 21:11:52 +0100

    added test script

---
+++ scripts/test_endpoints.py
@@ -0,0 +1,22 @@
+import requests
+
+URL = "http://localhost:6969/"
+
+GET_POLLS_BY_TITLE = f"{URL}/polls"
+GET_ALL_POLLS = f"{URL}/polls/all"
+
+def test_endpoint(url, expected_status):
+    response = requests.get(url)
+    assert response.status_code == expected_status, f"Test failed for {url}: Expected {expected_status}, got {response.status_code}"
+
+try:
+    test_endpoint(f"{GET_POLLS_BY_TITLE}", 404)
+    test_endpoint(f"{GET_POLLS_BY_TITLE}/{'TITLE'}", 200)
+    test_endpoint(f"{GET_POLLS_BY_TITLE}/{'TITLE'}", 200)
+    test_endpoint(GET_ALL_POLLS, 200)
+    
+    # Add more tests as needed
+    print("All tests passed!")
+except AssertionError as e:
+    print(e)
+    exit(1)
